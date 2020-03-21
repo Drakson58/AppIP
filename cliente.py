1e3b7120ab38dda5577adecf8615b4f1f6df95a6
Darkson <darksonbezerra123@gmail.com>
2020-03-20 22:50:40 -0300

    Falta juntar a interface com o cliente

--- cliente.py
+++ cliente.py
@@ -1,12 +1,15 @@
 import socket
+import interface
 
 host = ''
 porta = 5001
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect((host, porta)) # Se conecantando ao servidor.
-
+    
+    
 print("Para sair digite: 'sair'\n")
 while True:
+    
     # Enviando msg
     msg = input('Digite um Ip:')
     cliente.send(msg.encode())
---
+++ interface.py
@@ -0,0 +1,52 @@
+from tkinter import *
+
+
+def click():
+    entered_text = textentry.get()
+    saida.delete(0.0,END)
+    print(entered_text)
+
+    definicao = "Tente novamente" # Reposta do servidor.
+    saida.insert(END,definicao) # saida na tela // Reposta do servidor tem que vir aqui
+
+
+#main
+window = Tk()
+window.title("App_Josie_Roberto")
+window.configure(background = 'white')
+
+#Inserir foto na interface
+photo1 = PhotoImage(file = 'giphy.gif')
+Label(window,image = photo1,bg = 'black').grid(row = 0 ,column = 0 ,sticky = W )
+
+#Criar Label
+Label(window, text = 'Digite o seu número de IP:',bg = 'white',fg = 'black',font = 'none 12 bold').grid(row = 1,column = 0,sticky = W)
+
+#Entrada de texto
+textentry = Entry(window,width = 20, bg = 'white')
+textentry.grid(row = 2,column = 0,sticky = W)
+
+#Botão de enviar
+Button(window, text = 'ENVIAR', width = 6,command = click).grid(row = 3,column = 0,sticky = W)
+
+#Criar outra label
+Label(window, text = 'Definição:', bg = 'white', fg = 'black',font = 'none 12 bold').grid(row = 4,column = 0, sticky = W)
+
+#caixa de texto
+saida = Text(window,width = 60, height = 6, wrap = WORD , background = 'white')
+saida.grid(row = 5,column = 0, columnspan = 2 , sticky = W)
+
+#Dicionário
+#my_compdicionario = {'algoritimo' : 'Conjunto de passos para realizar determinada tarefa','bug': 'pedaço de código que ocasiona uma falha'}
+
+#função de saída
+def close_window():
+    window.destroy()
+    exit()
+
+#Encerrar
+Label(window,text = 'Clique para encerrar a conexão',bg = 'white' , fg = 'black' , font = 'none 12 bold' ).grid(row = 6,column = 0,sticky = W)
+Button(window,text = 'Encerrar', width = 14 , command = close_window).grid(row = 7 , column = 0, sticky = W)
+
+#Rodar o código
+window.mainloop()
